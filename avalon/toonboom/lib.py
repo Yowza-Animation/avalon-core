4af8b33875191672f0c95e33ef94e5877709231a
Aleks Berland <aleksb@yowzaanimation.com>
2020-07-31 17:26:29 -0400

    disable thread.daemon

--- avalon/toonboom/lib.py
+++ avalon/toonboom/lib.py
@@ -162,7 +162,7 @@ def launch_zip_file(filepath):
     # Launch Avalon server.
     self.server = Server(self.port)
     thread = threading.Thread(target=self.server.start)
-    thread.daemon = True
+    # thread.daemon = True
     thread.start()
 
     # Save workfile path for later.
