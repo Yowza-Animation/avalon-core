55b6920ae46c2303ef2e2f13b7959bd2818f3aa0
Aleks Berland <aleksb@yowzaanimation.com>
2020-08-11 08:26:08 -0400

    WIP

--- avalon/tools/creator/app.py
+++ avalon/tools/creator/app.py
@@ -356,7 +356,7 @@ class Window(QtWidgets.QDialog):
                 #     family,
                 #     subset_name
                 # ))
-                result.setText(family)
+                result.setText(subset_name)
 
             # Indicate subset existence
             if not subset_name:
