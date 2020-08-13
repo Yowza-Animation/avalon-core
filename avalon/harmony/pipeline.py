25e4dfefa59de800019cd14581f506ccbd299afd
Aleks Berland <aleksb@yowzaanimation.com>
2020-08-13 10:52:55 -0400

    WIP Harmony load template

--- avalon/harmony/pipeline.py
+++ avalon/harmony/pipeline.py
@@ -136,7 +136,8 @@ def containerise(name,
                  node,
                  context,
                  loader=None,
-                 suffix=None):
+                 suffix=None,
+                 nodes=[]):
     """Imprint node with metadata.
 
     Containerisation enables a tracking of version, author and origin
@@ -159,8 +160,9 @@ def containerise(name,
         "name": name,
         "namespace": namespace,
         "loader": str(loader),
-        "representation": str(context["representation"]["_id"])
-    }
+        "representation": str(context["representation"]["_id"]),
+        "nodes": nodes
+        }
 
     lib.imprint(node, data)
 
