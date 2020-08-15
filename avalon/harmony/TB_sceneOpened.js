4a2819451608b6aee4dd6b41946ec57c07c3090f
Aleks Berland <aleksb@yowzaanimation.com>
2020-08-14 20:12:59 -0400

    WIP

--- avalon/harmony/TB_sceneOpened.js
+++ avalon/harmony/TB_sceneOpened.js
@@ -40,7 +40,7 @@ function Client()
     {
       try
       {
-        var func = eval(request["function"]);
+        var func = eval.call($, request["function"]);
 
         if (request.args == null)
         {
