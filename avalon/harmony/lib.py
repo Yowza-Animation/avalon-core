5cae65d94ed831623bea15079a8ed4ece8ebf270
Aleks Berland <aleksb@yowzaanimation.com>
2020-08-22 15:32:23 -0400

    WIP

--- avalon/harmony/lib.py
+++ avalon/harmony/lib.py
@@ -72,11 +72,17 @@ def launch(application_path):
         zip_file = os.path.join(os.path.dirname(__file__), "temp.zip")
         launch_zip_file(zip_file)
 
+
+    new_work_path = os.getenv("AVALON_HARMONY_NEW_WORKFILE_PATH", "")
     self.callback_queue = queue.Queue()
 
     while True:
         main_thread_listen()
-
+        # This must be here to prevent race conditions
+        if new_work_path:
+            save_scene_as(get_local_harmony_path(new_work_path))
+            os.environ["AVALON_HARMONY_NEW_WORKFILE_PATH"] = ""
+            new_work_path = None
 
 def get_local_harmony_path(filepath):
     """From the provided path get the equivalent local Harmony path."""
@@ -144,15 +150,6 @@ def on_file_changed(path, threaded=True):
     """
 
     self.log.debug("File changed: " + path)
-
-    # This must be here to prevent race conditions
-    new_work_path = get_local_harmony_path(
-        os.getenv("AVALON_HARMONY_NEW_WORKFILE_PATH", "")
-    ).replace("\\", "/")
-    if new_work_path:
-        save_scene_as(new_work_path)
-        os.environ["AVALON_HARMONY_NEW_WORKFILE_PATH"] = ""
-        new_work_path = ""
 
     if self.workfile_path is None:
         return
